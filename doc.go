f3248371aaa9a7dcaaad1d2e9fef72e13cd97e92
Rasool Dastoori <rasool.dastoori@gmail.com>
2022-01-29 16:21:25 +0300

    Update the doc

--- doc.go
+++ doc.go
@@ -1,23 +1,4 @@
-// Package higgs is a tiny cross-platform Go library to hide or unhide files and directories
+// higgs is a tiny cross-platform Go library to hide or unhide files and directories
 //
-// 	package main
-//
-// 	import (
-// 		"fmt"
-// 		"github.com/dastoori/higgs"
-// 	)
-//
-// 	func main() {
-// 		err := higgs.Hide("foo.txt")
-//
-// 		if err != nil {
-// 			fmt.Println(err)
-// 		}
-//
-// 		err = higgs.Unhide("foo.txt")
-//
-// 		if err != nil {
-// 			fmt.Println(err)
-// 		}
-// 	}
+// **Supported OSs:** All unix based OSs (Tested on Ubuntu, MacOS), Windows
 package higgs
